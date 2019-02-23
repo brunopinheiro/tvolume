d2360d596252aec71ff4953247e34ad47b9bface
Bruno Pinheiro <brunobp07@gmail.com>
2019-02-23 22:37:45 +1100

    avoid getting 'missing value' as result

--- scripts/volume_percentage.sh
+++ scripts/volume_percentage.sh
@@ -11,12 +11,21 @@ is_muted() {
   [ $(osascript -e "output muted of (get volume settings)") == "true" ]
 }
 
+get_volume_output() {
+  local volume="$(osascript -e "output volume of (get volume settings)")"
+  if [ "$volume" == "missing value" ]; then
+    echo "-"
+  else
+    echo "$volume"
+  fi
+}
+
 get_volume_percentage() {
   if is_osx; then
     if is_muted; then
       echo "muted"
     else
-      echo "$(osascript -e "output volume of (get volume settings)")%"
+      echo "$(get_volume_output)%"
     fi
   else
     echo "Not Available"
